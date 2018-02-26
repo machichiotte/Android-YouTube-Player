5ef80458d18c22cee10fd6ee95a158c0d5930b33
eim <external.z08eidri@decathlon.com>
2018-02-26 15:07:34 +0100

    Added on back

--- YouTubePlayer/src/main/java/com/pierfrancescosoffritti/youtubeplayer/player/FullScreenActivity.java
+++ YouTubePlayer/src/main/java/com/pierfrancescosoffritti/youtubeplayer/player/FullScreenActivity.java
@@ -1,9 +1,11 @@
 package com.pierfrancescosoffritti.youtubeplayer.player;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -47,6 +49,30 @@ public class FullScreenActivity extends Activity {
         youTubePlayerView.playerUIControls.hideFullScreen();
     }
 
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)  {
+        if (Integer.parseInt(android.os.Build.VERSION.SDK) > 5
+                && keyCode == KeyEvent.KEYCODE_BACK
+                && event.getRepeatCount() == 0) {
+            Log.d("CDA", "onKeyDown Called");
+            onBackPressed();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        Log.d("CDA", "onBackPressed Called");
+       /* Intent setIntent = new Intent(Intent.ACTION_MAIN);
+        setIntent.addCategory(Intent.CATEGORY_HOME);
+        setIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(setIntent);*/
+
+       this.finish();
+    }
 }
 
 
